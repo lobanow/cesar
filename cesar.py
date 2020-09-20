c577a1a6eadf4679aaf787ac762c95933d316602
lobanow <71119059+lobanow@users.noreply.github.com>
2020-09-20 12:59:32 +0500

    Add files via upload
    шифрует текст введенный латиницей шифром Цезаря со смещением на 1 букву

---
+++ cesar.py
@@ -0,0 +1,62 @@
+
+b = input('b = ')
+a=''
+for i in range(len(b)):
+    if(b[i]=="a"):
+        a=a+"b"
+    elif(b[i]=="b"):
+        a=a+"c"
+    elif(b[i]=="c"):
+        a=a+"d"
+    elif(b[i]=="d"):
+        a=a+"e"
+    elif(b[i]=="e"):
+        a=a+"f"
+    elif(b[i]=="f"):
+        a=a+"g"
+    elif(b[i]=="g"):
+        a=a+"h"
+    elif(b[i]=="h"):
+        a=a+"i"
+    elif(b[i]=="i"):
+        a=a+"j"
+    elif(b[i]=="j"):
+        a=a+"k"
+    elif(b[i]=="k"):
+        a=a+"l"
+    elif(b[i]=="l"):
+        a=a+"m"
+    elif(b[i]=="m"):
+        a=a+"n"
+    elif(b[i]=="n"):
+        a=a+"o"
+    elif(b[i]=="o"):
+        a=a+"p"
+    elif(b[i]=="p"):
+        a=a+"q"
+    elif(b[i]=="q"):
+        a=a+"r"
+    elif(b[i]=="r"):
+        a=a+"s"
+    elif(b[i]=="s"):
+        a=a+"t"
+    elif(b[i]=="t"):
+        a=a+"u"
+    elif(b[i]=="u"):
+        a=a+"v"
+    elif(b[i]=="v"):
+        a=a+"w"
+    elif(b[i]=="w"):
+        a=a+"x"
+    elif(b[i]=="x"):
+        a=a+"y"
+    elif(b[i]=="y"):
+        a=a+"z"
+    elif(b[i]=="z"):
+        a=a+"a"
+    elif(b[i]==" "):
+        a=a+" "
+        
+print(a)
+
+c=input()
